2bbd4847251e9ea823271ce42ddef5b0fefef1bf
Rbrahma1245 <rbrahma1245@gmail.com>
2024-02-15 16:47:43 +0530

    fix button styling

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -2,30 +2,32 @@
 import { Component } from "react";
 
 class Pagination extends Component {
-    render() {
-      const { currentPage, totalPages, onPageChange } = this.props;
-      const pageNumbers = Array.from(
-        { length: totalPages },
-        (_, index) => index + 1
-      );
-  
-      return (
-        <div style={{marginTop:50}}>
-          {pageNumbers.map((number) => (
-            <span
-              key={number}
-              onClick={() => onPageChange(number)}
-              style={{
-                cursor: "pointer",
-                marginRight: "5px",
-                textDecoration: currentPage === number ? "underline" : "none",
-              }}
-            >
-              {number}
-            </span>
-          ))}
-        </div>
-      );
-    }
+  render() {
+    const { currentPage, totalPages, onPageChange } = this.props;
+    const pageNumbers = Array.from(
+      { length: totalPages },
+      (_, index) => index + 1
+    );
+
+    return (
+      <div style={{ marginTop: 50 }}>
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => onPageChange(number)}
+            style={{
+              cursor: "pointer",
+              marginRight: "5px",
+              textDecoration: currentPage === number ? "underline" : "none",
+              background: currentPage === number ? "#535C91" : "none",
+              outline:"none"
+            }}
+          >
+            {number}
+          </button>
+        ))}
+      </div>
+    );
   }
-  export default Pagination
+}
+export default Pagination;
